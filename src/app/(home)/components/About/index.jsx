d52eb5a3a79dc9c0d0ed1da274f275b9bcd9a42a
Washington <washington.rodrigues@redgtech.com.br>
2024-01-11 02:46:23 -0300

    feat: add the about section to HomePage

---
+++ src/app/(home)/components/About/index.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import styles from './styles.module.css'
+import Link from 'next/link';
+import BlackBtn from '../../../../components/ui/black-btn'
+
+const About = () => {
+    return (
+        <section className={`${styles.section_about} relative`}>
+            <div className="text-center">
+                <h1 className={`${styles.h1}`}>Nexus</h1>
+                <h2 className={`${styles.h2}`}>Quem somos?</h2>
+            </div>
+            <ul className={`${styles.ul}`}>
+                <li className={`${styles.li}`}>
+                    <Image src="/brazilsvg.svg" alt='Clientes por todo Brasil' width={30} height={30} sizes='' />
+                    <p>Clientes por todo Brasil</p>
+                </li>
+                <li className={`${styles.li}`}>
+                    <Image src="/plussvg.svg" alt='Clientes por todo Brasil' width={30} height={30} sizes='' />
+                    <p>03 anos de mercado</p>
+                </li>
+                <li className={`${styles.li}`}>
+                    <Image src="/plussvg.svg" alt='Clientes por todo Brasil' width={30} height={30} sizes='' />
+                    <p>100k faturados por clientes</p>
+                </li>
+            </ul>
+            <p className={`${styles.paragraph}`}>A Nexus, fundada em 2020 por Timóteo Levi e Washington Rodrigues, é uma agência que une paixão por dados, tecnologia e marketing digital. Comprometida em gerar valor para clientes, destacamo-nos na gestão de tráfego no Meta e Google, além de oferecer desenvolvimento personalizado de sites e sistemas web. Ideal para pequenas e médias empresas em busca de crescimento digital e resultados mensuráveis.</p>
+
+            {/* <Link href="#" className={`${styles.btn}`}>Saiba mais</Link> */}
+            <BlackBtn href="" title="Saiba mais" />
+            <Image className='absolute bottom-0 right-0' src="/aboutbgimg.png" alt='' width={132} height={160} sizes='' style={{ opacity: ".3" }} />
+        </section >
+    );
+}
+
+export default About;
---
+++ src/components/ui/black-btn.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+const BlackBtn = ({ title, href }) => {
+    const buttonStyle = {
+        backgroundColor: "#000",
+        color: "#fff",
+        textTransform: "uppercase",
+        fontFamily: "poppins",
+        fontWeight: "bold",
+        padding: "13px 28px",
+        borderRadius: "5px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "20px auto",
+        width: "150px",
+    }
+    return (
+        <Link href={href} style={buttonStyle}>{title}</Link>
+    );
+}
+
+export default BlackBtn;

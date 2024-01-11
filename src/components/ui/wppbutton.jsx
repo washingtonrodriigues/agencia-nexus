224646a292a2228ce15300ef91f7b4733ec7849f
Washington <washington.rodrigues@redgtech.com.br>
2024-01-11 02:44:22 -0300

    feat: create the flying button of the whatsapp

---
+++ src/components/ui/wppbutton.jsx
@@ -0,0 +1,53 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const WhatsappBtn = () => {
+    const [isShaking, setIsShaking] = useState(false);
+
+    useEffect(() => {
+        const shakeButton = () => {
+            setIsShaking(true);
+
+            // Remova a classe após a conclusão da animação
+            setTimeout(() => {
+                setIsShaking(false);
+            }, 500);
+        };
+
+        // Chame a função shakeButton a cada 5 segundos
+        const intervalId = setInterval(shakeButton, 5000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    const buttonStyle = {
+        zIndex: 9999,
+        bottom: "20px",
+        backgroundColor: "rgb(50, 168, 82)",
+        borderRadius: "50%",
+        padding: "10px",
+        animation: isShaking ? 'shake 0.5s ease-in-out' : 'none',
+    };
+
+    return (
+        <Link
+
+            href="#"
+            className="whatsapp-button fixed right-4 flex h-[65px] w-[65px] items-center justify-center"
+            style={buttonStyle}
+        >
+            <Image
+                src="/wppsvg.svg"
+                alt="Whatsapp"
+                width={35}
+                height={35}
+                sizes=""
+            />
+        </Link>
+    );
+}
+
+export default WhatsappBtn;
